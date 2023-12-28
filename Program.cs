a88759234b82aba5e9bcac06438c720407999e14
Gabrielle Almeida de Oliveira <gabialmeidaoliveira12@gmail.com>
2023-12-27 22:17:05 -0300

    Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero:"123456",modelo:"Nokia G11", imei: "111111", memoria: 64 );
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatssap");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iPhone = new Iphone(numero:"654321",modelo:"iPhone X", imei: "222222", memoria: 128 );
+iPhone.ReceberLigacao();
+iPhone.InstalarAplicativo("Telegram");
